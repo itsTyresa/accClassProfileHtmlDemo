d0c9c02df70f3433779c28a269d00fdf282f7f6a
itsTyresa <126362468+itsTyresa@users.noreply.github.com>
2023-03-16 19:08:58 -0700

    Update students.js
    fixed the city

--- students.js
+++ students.js
@@ -9,7 +9,7 @@ export const data = [
     {
         firstName:'Tyresa',
         lastName:'Theus',
-        location: 'California, CA',
+        location: 'San Jacinto, CA',
         html:'tyresaTheus.html',
         imagePath:'images/tyresaTheus.jpg'
     },
