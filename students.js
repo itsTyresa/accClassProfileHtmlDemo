f813289dc4ca255e40412e894253bfae91f4c226
itsTyresa <126362468+itsTyresa@users.noreply.github.com>
2023-03-16 19:02:20 -0700

    Update students.js
    added my own information

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Tyresa',
+        lastName:'Theus',
+        location: 'California, CA',
+        html:'tyresatheus.html',
+        imagePath:'images/tyresaTheus.jpg'
     },
 ]
 
